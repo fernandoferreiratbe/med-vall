ffcd80b2b61939eb28b72e48b202f06fc7a92488
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2022-12-22 08:30:27 -0300

    Initial commit

---
+++ src/test/java/io/github/fernandoferreira/medvoll/api/MedvollApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fernandoferreira.medvoll.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedvollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
