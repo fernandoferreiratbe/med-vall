ddabe2659f1e387271ef773693e8dc9a5431555e
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2023-01-25 14:55:14 -0300

    Create doctor and address entities
    - Create doctor and address entities annotated with JPA and Lombok annotations.

---
+++ src/main/java/io/github/fernandoferreira/medvoll/api/domain/Address.java
@@ -0,0 +1,22 @@
+package io.github.fernandoferreira.medvoll.api.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    private String street;
+    private String district;
+    private String cep;
+    private String city;
+    private String uf;
+    private String additionalInfo;
+    private String number;
+
+}
---
+++ src/main/java/io/github/fernandoferreira/medvoll/api/domain/doctor/Doctor.java
@@ -0,0 +1,28 @@
+package io.github.fernandoferreira.medvoll.api.domain.doctor;
+
+import io.github.fernandoferreira.medvoll.api.domain.Address;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "doctors")
+@Entity(name = "Doctor")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Doctor {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String crm;
+    @Enumerated(EnumType.STRING)
+    private Specialty specialty;
+    @Embedded
+    private Address address;
+
+}
